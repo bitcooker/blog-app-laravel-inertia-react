a2889258b0de30ac3cfc9e1d2d289068f7e6877f
bitcooker <dreamhighpassionhigh@gmail.com>
2023-04-23 19:53:34 +0800

    feat: edit StorePostRequest

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -11,7 +11,7 @@ class StorePostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class StorePostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required',
+            'description' => 'required'
         ];
     }
 }
