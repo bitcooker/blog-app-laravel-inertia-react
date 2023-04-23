3b957012b9656c4c5d14cad345b97d611dfea664
bitcooker <dreamhighpassionhigh@gmail.com>
2023-04-23 21:24:36 +0800

    feat: React View (Index) for all posts

---
+++ resources/js/Pages/Post/Index.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Inertia } from '@inertiajs/inertia';
+import { InertiaLink, usePage } from '@inertiajs/inertia-react';
+
+const Index = () => {
+    const { posts } = usePage().props;
+    const { data } = posts;
+
+    return (
+        <div>
+            <div className="container mx-auto">
+                <h1 className="mb-8 text-3xl font-bold text-center">Post</h1>
+                <div className="flex items-center justify-between mb-6">
+                    <InertiaLink
+                        className="px-6 py-2 text-white bg-green-500 rounded-md focus:outline-none"
+                        href={route("posts.create")}
+                    >
+                        Create Post
+                    </InertiaLink>
+                </div>
+
+                <div className="overflow-x-auto bg-white rounded shadow">
+                    <table className="w-full whitespace-nowrap">
+                        <thead className="text-white bg-gray-600">
+                            <tr className="font-bold text-left">
+                                <th className="px-6 pt-5 pb-4">#</th>
+                                <th className="px-6 pt-5 pb-4">Title</th>
+                                <th className="px-6 pt-5 pb-4">Description</th>
+                                <th className="px-6 pt-5 pb-4">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map(({ id, title, description }) => (
+                                <tr key={id} className="">
+                                    <td className="border-t">
+                                        <InertiaLink
+                                            href={route("posts.edit", id)}
+                                            className="flex items-center px-6 py-4 focus:text-indigo-700 focus:outline-none"
+                                        >
+                                            {id}
+                                        </InertiaLink>
+                                    </td>
+                                    <td className="border-t">
+                                        <InertiaLink
+                                            href={route("posts.edit", id)}
+                                            className="flex items-center px-6 py-4 focus:text-indigo-700 focus:outline-none"
+                                        >
+                                            {title}
+                                        </InertiaLink>
+                                    </td>
+                                    <td className="border-t">
+                                        <InertiaLink
+                                            tabIndex="1"
+                                            className="flex items-center px-6 py-4"
+                                            href={route("posts.edit", id)}
+                                        >
+                                            {description}
+                                        </InertiaLink>
+                                    </td>
+                                    <td className="border-t">
+                                        <InertiaLink
+                                            tabIndex="1"
+                                            className="px-4 py-2 text-sm text-white bg-blue-500 rounded"
+                                            href={route("posts.edit", id)}
+                                        >
+                                            Edit
+                                        </InertiaLink>
+                                    </td>
+                                </tr>
+                            ))}
+                            {data.length === 0 && (
+                                <tr>
+                                    <td
+                                        className="px-6 py-4 border-t"
+                                        colSpan="4"
+                                    >
+                                        No contacts found.
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
