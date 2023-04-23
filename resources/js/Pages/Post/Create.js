85b96241358362d46a31a63b774cbd9f642c20e9
bitcooker <dreamhighpassionhigh@gmail.com>
2023-04-23 21:16:55 +0800

    feat: React view (Create) for crud

---
+++ resources/js/Pages/Post/Create.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Inertia } from '@inertiajs/inertia';
+import { InertiaLink, useForm} from '@inertiajs/inertia-react';
+
+const Create = ()=> {
+    const {data, setData, errors, post} = useForm({
+        title: "",
+        description: "",
+    });
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        post(route("posts.store"));
+    }
+
+    return (
+        <div className="mt-20">
+            <div className="container flex flex-col justify-center mx-auto">
+                <div>
+                    <h1 className="mb-8 text-3xl font-bold">
+                        <InertiaLink
+                            href={route("posts.index")}
+                            className="text-indigo-600 hover:text-indigo-700"
+                        >
+                            Posts
+                        </InertiaLink>
+                        <span className="font-medium text-indigo-600"> / </span>
+                        Create
+                    </h1>
+                </div>
+                <div className="max-w-6xl p-8 bg-white rounded shadow">
+                    <form name="createForm" onSubmit={handleSubmit}>
+                        <div className="flex flex-col">
+                            <div className="mb-4">
+                                <label className="">Title</label>
+                                <input
+                                    type="text"
+                                    className="w-full px-4 py-2"
+                                    label="Title"
+                                    name="title"
+                                    value={data.title}
+                                    onChange={(e) =>
+                                        setData("title", e.target.value)
+                                    }
+                                />
+                                <span className="text-red-600">
+                                    {errors.title}
+                                </span>
+                            </div>
+                            <div className="mb-0">
+                                <label className="">Description</label>
+                                <textarea
+                                    type="text"
+                                    className="w-full rounded"
+                                    label="description"
+                                    name="description"
+                                    errors={errors.description}
+                                    value={data.description}
+                                    onChange={(e) =>
+                                        setData("description", e.target.value)
+                                    }
+                                />
+                                <span className="text-red-600">
+                                    {errors.description}
+                                </span>
+                            </div>
+                        </div>
+                        <div className="mt-4">
+                            <button
+                                type="submit"
+                                className="px-6 py-2 font-bold text-white bg-green-500 rounded"
+                            >
+                                Save
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
